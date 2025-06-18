66d0cb4f5b5229e06a7bbd0a8680a83368956d9c
MochIqbalFirmansyah-ID <22090053.iqbal@student.poltektegal.ac.id>
2025-06-18 13:39:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ st.set_page_config(page_title="Dashboard ScanSek", layout="wide")
 
 # Koneksi ke MongoDB
 client = MongoClient('mongodb://iqbal:passwordkuaman@70.153.17.43:27017/beritascansek_db?authSource=beritascansek_db')
-db = client['scrap']
-collection = db['daftar_berita']
+db = client['beritascansek_db']
+collection = db['berita_kesehatan']
 
 # Ambil data dari MongoDB
 data = list(collection.find())
